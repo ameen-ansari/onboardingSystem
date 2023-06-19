e596b1f07587f9f21f5cf58c350b3915bcb735e4
ameen <ameen55668@gmail.com>
2023-06-19 10:47:06 +0500

    add hr security system

--- server.js
+++ server.js
@@ -11,17 +11,47 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('welcome');
-    // res.send(process.env.USERS);
 })
 app.post('/auth/check', (req, res) => {
-    if (req.body.email === 'ameenansari@gmail.com' || req.body.email === 'asmarnaeem@gmail.com') {
+    if (req.body.email === 'hr@gmail.com') {
         if (req.body.password === '123456') {
-            res.send('you r admin')
+            res.send({
+                user: 'hr',
+                message: 'user authrized'
+            })
         } else {
-            res.send('wrong password')
+            res.send({
+                message: 'wrong password'
+            })
+        }
+    } else if (req.body.email === 'tech@gmail.com') {
+        if (req.body.password === '123456') {
+            res.send({
+                user: 'tech',
+                message: 'user authrized'
+            })
+        } else {
+            res.send({
+                message: 'wrong password'
+            })
+        }
+    }
+    else if (req.body.email === 'qa@gmail.com') {
+        if (req.body.password === '123456') {
+            res.send({
+                user: 'qa',
+                message: 'user authrized'
+            })
+        } else {
+            res.send({
+                message: 'wrong password'
+            })
         }
     } else {
-        res.send('you r not a admin')
+        res.send({
+            user: 'unauthrized user',
+            message: 'user not found'
+        })
     }
 })
 
